ecaccae367e43f6cebd40193b6c83d02bf795f30
Parvathi-99 <deviparvathi111@gmail.com>
2025-05-05 12:57:01 +0530

    Update Streamlit_app.py
    requests changes

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: {st.__version__}")
 st.write(
@@ -36,3 +38,6 @@ time_to_insert=st.button("Submit Order")
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success(f"Your Smoothie is ordered,{name_on_order} !",icon="✅") 
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
